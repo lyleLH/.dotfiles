6342d6b7f0de47cb2066a35404cb643f490b0f3f
Major Tom <v2top1@163.com>
2023-03-27 00:49:28 +0800

    tmux kitty配置

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,14 +1,25 @@
 
-alias vim="nvim"
-alias vide="/Applications/Neovide.app/Contents/MacOS/neovide"
+# alias vim="nvim"
+alias nv="nvim"
+# alias vide="neovide --frame none"
+
+# alias vide="neovide"
+alias vv="/Applications/Neovide.app/Contents/MacOS/neovide"
+alias vimr="~/vimr"
 alias myip="curl http://ipecho.net/plain; echo"
 
 alias sexcode14="sudo xcode-select -s /Applications/Xcode.app/Contents/Developer"
 alias sexcode13="sudo xcode-select -s /Applications/Xcode13.1/Xcode.app/Contents/Developer"
 alias showxcodepath="xcode-select -p"
 
-alias showalias="vim ~/.config/zsh/alias.zsh"
-alias xvim="vim ~/.xvimrc"
+alias xvim="vi ~/.xvimrc"
 
-alias keyboard="vim ~/.config/goku_config/karabiner.edn"
-alias ivim="vim ~/.ideavimrc"
+alias keyboard="vi ~/.config/goku_config/karabiner.edn"
+alias vimkeys="vi ~/.config/nvim/lua/haoliu/core/keymaps.lua"
+
+alias ivim="vi ~/.ideavimrc"
+
+alias skhdkey="vi ~/.config/skhd/skhdrc"
+
+alias showalias="nv ~/.config/zsh/alias.zsh"
+alias tmuxconf="nv ~/.config/tmux/.tmux.conf"
