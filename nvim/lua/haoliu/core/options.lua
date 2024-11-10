55f9c40300f5d3c2e17d142e8530c78ef80f4bf3
Hao Liu <v2top1@163.com>
2024-11-10 13:30:30 -0800

    --

--- nvim/lua/haoliu/core/options.lua
+++ nvim/lua/haoliu/core/options.lua
@@ -1,41 +1,30 @@
-local opt = vim.opt -- for conciseness
-opt.guicursor = ""
--- line numbers
-opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 4 -- 2 spaces for indent width
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
-opt.shiftwidth = 4
 
--- line wrapping
-opt.wrap = false -- disable line wrapping
-opt.scrolloff = 8
+opt.wrap = false
+
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-opt.hlsearch = false
-opt.incsearch = true
-opt.smartindent = true
--- cursor line
-opt.cursorline = true -- highlight the current cursor line
 
-opt.guifont = { "CaskaydiaCover Nerd Font", ":h20" }
+opt.cursorline = true
 
--- opt.guifont = "Cascadia Code:h13"
-
--- opt.guifont = { "Fira Code Font", ":h20" }
-
--- appearance
--- turn on termguicolors for nightfly colorscheme to work
+-- turn on termguicolors for tokyonight colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
-opt.colorcolumn = "80"
+
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
@@ -46,5 +35,5 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
-opt.iskeyword:append("-") -- consider string-string as whole word
-opt.cmdheight = 1
+-- turn off swapfile
+opt.swapfile = false
